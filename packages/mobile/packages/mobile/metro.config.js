c7af69ffb835b2734ca3810ec859c140d83ef9ce
mohanadyoussefmy-png <mohanad.youssef.my@gmail.com>
2026-09-06 05:04:22 +0300

    Delete packages/mobile/packages/mobile directory

--- packages/mobile/packages/mobile/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig, mergeConfig } = require('metro-config');
-
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig(__dirname);
-  return mergeConfig(defaultConfig, {});
-})();
